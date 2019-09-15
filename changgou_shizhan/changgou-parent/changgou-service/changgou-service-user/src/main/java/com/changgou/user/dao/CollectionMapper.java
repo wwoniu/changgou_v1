0355eada72b8702e146895846e698fedefe4505e
little-s <1187034994@qq.com>
2019-09-15 16:09:13 +0800

    用户收藏与足迹

---
+++ changgou_shizhan/changgou-parent/changgou-service/changgou-service-user/src/main/java/com/changgou/user/dao/CollectionMapper.java
@@ -0,0 +1,7 @@
+package com.changgou.user.dao;
+
+import com.changgou.user.pojo.Collection;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface CollectionMapper extends Mapper<Collection> {
+}
